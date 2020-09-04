4d931aa3f69150ffb573054cf443ba0d76eb8f26
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-09-05 04:36:46 +0900

    change test makefile

--- test/Makefile_avx2.mk
+++ test/Makefile_avx2.mk
@@ -2,8 +2,8 @@ DD_AVX_DIR?=$(HOME)/lib/dd_avx
 
 CXX=g++
 RPATH=-Wl,-rpath,$(DD_AVX_DIR)/lib 
-LIB=-I$(DD_AVX_DIR)/include -L$(DD_AVX_DIR)/lib -lqd -ldd-avx -lqd
-CXXFLAGS=-O3 -std=c++14 -fopenmp -lm -DUSE_AVX2
+LIB=-I$(DD_AVX_DIR)/include -L$(DD_AVX_DIR)/lib -ldd-avx -lqd
+CXXFLAGS=-O3 -std=c++14 -fopenmp -lm 
 
 all: $(FUNC).out 
 
